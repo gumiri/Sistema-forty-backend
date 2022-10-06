91cda5ae6a66b13b1ee3e17cec575a36ee790d25
FortyFlex <marketing@fortyflex.com.br>
2022-10-06 12:02:54 -0300

    Trocar arquitetura quase toda...

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ const express = require('express');
 const app = express();
 
 
-const testeController = require('./controller/cliRecebAbertoController');
 const copiarBdsController = require('./controller/copiarBdsController');
-const gCliente = require('./controller/gClientesController');
 
-app.use("/teste", testeController);
 
 app.use("/copiar", copiarBdsController);
 
-app.use('/cliente', gCliente);
 
 
 module.exports = app;
